1e9f72cafa37f8315ca56c4bafbd168e05df67b6
adit negi <aditnegi1@gmail.com>
2023-04-17 11:16:19 -0500

    add cors policy

--- web-server/services/web/project/__init__.py
+++ web-server/services/web/project/__init__.py
@@ -56,6 +56,13 @@ class ResearchPapers(db.Model):
 
 
 #application code
+@app.after_request
+def add_cors_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 
 BASE_URL = "/v1/api/"
 @app.route("/")
